429231a9a12540026dcbfc81691fad8318dd7472
Gerry&Tech <davidecorsaro2009@gmail.com>
2026-02-24 17:10:56 +0100

    Update apps.js

--- apps/apps.js
+++ apps/apps.js
@@ -11,6 +11,117 @@ const APPS = [
     demo: "https://www.youtube.com/watch?v=O9AB-pAx9fA",
     downloads: 928,
     level: "beginner",
+    estTime: "20 min",
+    complexity: "Easy",
+
+    codePreviewCpp:
+      `#include <Arduino_RouterBridge.h>
+#include <Servo.h>
+
+Servo myservo;
+
+int ledR = 2;
+int ledG = 3;
+int ledV = 4;
+int pulsante = 5;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledV, OUTPUT);
+  pinMode(pulsante, INPUT);
+  myservo.attach(9);
+  Bridge.begin();
+  Bridge.provide("caso1", caso1);
+  Bridge.provide("caso2", caso2);
+  Bridge.provide("caso3", caso3);
+  Bridge.provide("valore", valore);
+  Bridge.provide("spegni", spegni);
+  Bridge.provide("suspence", suspence);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+
+void caso1(){
+  digitalWrite(ledR, HIGH);
+  myservo.write(0);
+}
+
+void caso2(){
+  digitalWrite(ledG, HIGH);
+  myservo.write(90);
+}
+
+void caso3(){
+  digitalWrite(ledV, HIGH);
+  myservo.write(180);
+}
+
+int valore(){
+  return digitalRead(pulsante);
+}
+
+void spegni(){
+  digitalWrite(ledR, LOW);
+  digitalWrite(ledG, LOW);
+  digitalWrite(ledV, LOW);
+  myservo.write(0);
+}
+
+void suspence(){
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(ledR, HIGH);
+    delay(250);
+    digitalWrite(ledR, LOW);
+    delay(50);
+    digitalWrite(ledG, HIGH);
+    delay(250);
+    digitalWrite(ledG, LOW);
+    delay(50);
+    digitalWrite(ledV, HIGH);
+    delay(250);
+    digitalWrite(ledV, LOW);
+    delay(50);
+    }
+}`,
+
+    codePreviewPy:
+      `import time
+import random
+from arduino.app_utils import App
+from arduino.app_utils import Bridge
+
+
+
+def loop():
+    """This function is called repeatedly by the App framework."""
+    # You can replace this with any code you want your App to run repeatedly.
+    if (Bridge.call("valore") == 0):
+        val = random.randint(1, 11)
+        Bridge.call("suspence")
+        if ((val >= 1) and (val <6)):
+            Bridge.call("caso1")
+            time.sleep(3)
+            
+        if ((val >= 6) and (val <9)):
+            Bridge.call("caso2")
+            time.sleep(3)
+
+        if (val >= 9):
+            Bridge.call ("caso3")
+            time.sleep(3)
+    
+    Bridge.call("spegni")
+    time.sleep(0.2)
+
+
+# See: https://docs.arduino.cc/software/app-lab/tutorials/getting-started/#app-run
+App.run(user_loop=loop)`
   },
   {
     id: "display-a-7-segmenti",
